9ef32e45230cc1532f9fe5f5765342164f5979ea
Kai Lam Kingsle U <ukingsle24@gmail.com>
2023-03-06 10:07:44 -0800

    Fixed compilation errors on mac, added gpt api on base.py

--- backend/base.py
+++ backend/base.py
@@ -1,4 +1,6 @@
+from urllib import response
 from flask import Flask
+import openai
 
 api = Flask(__name__)
 
@@ -6,7 +8,20 @@ api = Flask(__name__)
 def my_profile():
     response_body = {
         "name": "Kingsley",
-        "about" :"Hello, I am an anspiring web developer!"
+        "about" :"Hello, I am an aspiring web developer!"
+
     }
 
     return response_body
+
+# Below is testing for GPT APIs using Astronomical Observatories (art gen)
+def gpt_profile():
+    response_generator = {
+    openai.Completion.create(
+    engine="davinci",
+    prompt="Make a list of astronomical observatories:"
+    )
+    }
+
+    return response_generator
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,13 @@ export default function Header() {
   return (
     <div className={styles.header}>
       <Button variant="outlined" size="Large">
-        Large
+        Button1
       </Button>
+      <Button
+      Button variant="outlined" size="Large">
+        Button2
+      </Button>
+
     </div>
   );
 }
